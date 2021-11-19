203a5bf6023af37594f81edeb9a934da4ea9d556
Seva Alekseyev <sevaa@sprynet.com>
2021-11-18 19:28:48 -0500

    Ver bump

--- dwex/__main__.py
+++ dwex/__main__.py
@@ -8,7 +8,7 @@ from .tree import DWARFTreeModel, has_code_location, cu_sort_key
 from .scriptdlg import ScriptDlg
 from .ui import setup_ui
 
-version = (1, 22)
+version = (1, 23)
 
 # TODO:
 # On MacOS, start without a main window, instead show the Open dialog
