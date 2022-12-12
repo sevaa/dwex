add414f7411d47b846d0a98689a697dfa6e4eb03
Seva Alekseyev <sevaa@nih.gov>
2022-12-12 15:36:28 -0500

    Support for --user

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from subprocess import run
 from setuptools import setup
-from setuptools.command.install import install, uninstall
-import platform, sys, os
+from setuptools.command.install import install
+import platform, sys, os, site
 from os import path, environ
 
 #------------------------------------
@@ -18,6 +18,8 @@ def create_shortcut_under(root, exepath):
 def create_shortcut():
     try:
         exepath = path.join(path.dirname(sys.executable), "Scripts", "dwex.exe")
+        if not path.exists(exepath):
+            exepath = path.join(path.dirname(site.getusersitepackages()), "Scripts", "dwex.exe")
         if not create_shortcut_under('ALLUSERSPROFILE', exepath):
             create_shortcut_under('APPDATA', exepath)
     except:
@@ -46,18 +48,6 @@ def register_desktop_app():
     except:
         pass
 
-def register_desktop_app():
-    try:
-        import base64
-        with open('/usr/share/applications/dwex.desktop', 'w') as f:
-            f.write("[Desktop Entry]\nVersion=1.1\nType=Application\nName=DWARF Explorer\nComment=Debug information visualizer\nExec=dwex\nTerminal=false\nIcon=dwex\nCategories=Development;Debugger;\n")
-        with open('/usr/share/icons/hicolor/48x48/apps/dwex.png', 'wb') as f:
-            f.write(base64.b64decode("iVBORw0KGgoAAAANSUhEUgAAADAAAAAwBAMAAAClLOS0AAAAKnRFWHRDcmVhdGlvbiBUaW1lANHhIDYg7e7/IDIwMjEgMTM6MDg6NDcgLTA1MDBuo0qzAAAAB3RJTUUH5QsGEQ8VL0d/PwAAAAlwSFlzAAALEgAACxIB0t1+/AAAAARnQU1BAACxjwv8YQUAAAAGUExURf///wAAAFXC034AAACkSURBVHjavdNREoUgCAXQyw5g/5sNxEoEpvc+yim0OcUoJvBRE2sAaeQRAkgB0NdBbJcPfgMNbglI8w9AAu1tDjXQv8DEUgH1gAwaDKQEu3kDWzBVIBNCReaG+Fc7oIAvGt1/g61cnsGnaM9nn0B89Rloq9UF4JDJd9VHsSILSIR7jiHTAm0qbiHNau7StlEXlCU5T0ALS65Zdh5lp+VwtvByOwCIiA5ALXz03AAAAABJRU5ErkJggg=="))
-        import subprocess
-        subprocess.call('update-desktop-database')
-    except:
-        pass    
-
 #--------------------------------------
 
 class my_install(install):
@@ -67,21 +57,6 @@ class my_install(install):
             create_shortcut()
         elif platform.system() == 'Linux':
             register_desktop_app()
-
-class my_uninstall(uninstall):
-    def run(self):
-        uninstall.run(self)
-        if platform.system() == 'Windows':
-            delete_shortcut('ALLUSERSPROFILE')
-            delete_shortcut('APPDATA')
-        elif platform.system() == 'Linux':
-            safe_remove('/usr/share/applications/dwex.desktop')
-            safe_remove('/usr/share/icons/hicolor/48x48/apps/dwex.png')
-            try:
-                import subprocess
-                subprocess.call('update-desktop-database')
-            except:
-                pass
 
 # Pull the long desc from the readme
 try:
@@ -96,7 +71,7 @@ setup(
     packages=['dwex'],
     url="https://github.com/sevaa/dwex/",
     entry_points={"gui_scripts": ["dwex = dwex.__main__:main"]},
-    cmdclass={'install': my_install, 'uninstall': my_uninstall},
+    cmdclass={'install': my_install},
     keywords = ['dwarf', 'debug', 'debugging', 'symbols', 'viewer', 'view', 'browser', 'browse', 'tree'],
     license="BSD",
     author="Seva Alekseyev",
