6b1bf5760bf3af4d2e654846697dcd30e6888e82
Seva Alekseyev <valekseyev@niaid.nih.gov>
2023-08-21 16:50:02 -0400

    Locals

--- dwex/exprutil.py
+++ dwex/exprutil.py
@@ -1,25 +1,23 @@
 from elftools.dwarf.descriptions import _REG_NAMES_x86, _REG_NAMES_x64, _REG_NAMES_AArch64
 from elftools.dwarf.dwarf_expr import DWARFExprOp
 
+# TODO: take from pyelftools when they publish
 # Source: https://github.com/ARM-software/abi-aa/blob/main/aadwarf32/aadwarf32.rst#dwarf-register-names
 _REG_NAMES_ARM = [
     'r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7',
     'r8', 'r9', 'r10', 'r11', 'r12', 'sp', 'lr', 'pc'
 ] + ['<none>']*48 + ["s%d" %(n,) for n in range(32)] + [
     'f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7',
-    'wcgr0', 'wcgr1', 'wcgr2', 'wcgr3', 'wcgr4', 'wcgr5', 'wcgr6', 'wcgr7', 
-    'acc0', 'acc1', 'acc2', 'acc3', 'acc4', 'acc5', 'acc6', 'acc7',
+    'acc0', 'acc1', 'acc2', 'acc3', 'acc4', 'acc5', 'acc6', 'acc7', #AKA wcgr0..7
     'wr0', 'wr1', 'wr2', 'wr3', 'wr4', 'wr5', 'wr6', 'wr7',
     'wr8', 'wr9', 'wr10', 'wr11', 'wr12', 'wr13', 'wr14', 'wr15',
-    'spsr', 'spsr_fiq', 'spsr_irq', 'spsr_abt', 'spsr_und', 'spsr_svc'] + ['<none>']*14 + [
+    'spsr', 'spsr_fiq', 'spsr_irq', 'spsr_abt', 'spsr_und', 'spsr_svc'] + ['<none>']*9 + [
     'ra_auth_code', 'r8_usr', 'r9_usr', 'r10_usr', 'r11_usr', 'r12_usr', 'r13_usr', 'r14_usr',
     'r8_fiq', 'r9_fiq', 'r10_fiq', 'r11_fiq', 'r12_fiq', 'r13_fiq', 'r14_fiq',
-    'r8_irq', 'r9_irq', 'r10_irq', 'r11_irq', 'r12_irq', 'r13_irq', 'r14_irq',
-    'r8_abt', 'r9_abt', 'r10_abt', 'r11_abt', 'r12_abt', 'r13_abt', 'r14_abt',
-    'r8_und', 'r9_und', 'r10_und', 'r11_und', 'r12_und', 'r13_und', 'r14_und',
-    'r8_svc', 'r9_svc', 'r10_svc', 'r11_svc', 'r12_svc', 'r13_svc', 'r14_svc'] + ['<none>']*24 + [
-    'wc0', 'wc1', 'wc2', 'wc3', 'wc4', 'wc5', 'wc6', 'wc7'] + ['<none>']*55 + [
-        "d%d" %(n,) for n in range(31)] + ['<none>']*21 + [
+    'r13_irq', 'r14_irq', 'r13_abt', 'r14_abt',
+    'r13_und', 'r14_und', 'r13_svc', 'r14_svc'] + ['<none>']*26 + [
+    'wc0', 'wc1', 'wc2', 'wc3', 'wc4', 'wc5', 'wc6', 'wc7'] + ['<none>']*56 + [
+        "d%d" %(n,) for n in range(32)] + ['<none>']*32 + [
      'tpidruro', 'tpidrurw', 'tpidpr', 'htpidpr'
 ]
 
@@ -27,8 +25,33 @@ _REG_NAMES_MIPS = [
     '$zero', '$at', '$v0', '$v1', '$a0', '$a1', '$a2', '$a3',
     '$t0', '$t1', '$t2', '$t3', '$t4', '$t5', '$t6', '$t7',
     '$s0', '$s1', '$s2', '$s3', '$s4', '$s5', '$s6', '$s7',
-    '$t8', '$t9', '$k0', '$k1', '$gp', '$sp', '$fp', '$ra'
-] # Lo, hi???
+    '$t8', '$t9', '$k0', '$k1', '$gp', '$sp', '$fp', '$ra',
+    '$ps', '$lo', '$hi', '$badvaddr', '$cause', '$pc'] + [
+        '$fp%d' % n for n in range(35)] + ['<none>'] + [
+        '$cp%d' % n for n in range(15)] + ['$prid']
+
+# Source: https://github.com/riscv-non-isa/riscv-elf-psabi-doc/blob/master/riscv-dwarf.adoc
+_REG_NAMES_RISCV = [
+    'zero', 'ra', 'sp', 'gp', 'tp', 't0', 't1', 't2',
+    'fp', 's1', 'a0', 'a1', 'a2', 'a3', 'a4', 'a5',
+    'a6', 'a7', 's2', 's3', 's4', 's5', 's6', 's7',
+    't8', 't9', 's10', 's11', 't3', 't4', 't5', 't6'] + [
+        'f%d' % n for n in range(32)] + ['afrc'] + ['<none>']*31 + [
+        'v%d' % n for n in range(32)]
+
+# Source for 64 bit: https://github.com/riscv-non-isa/riscv-elf-psabi-doc/blob/master/riscv-dwarf.adoc
+# Source for 32 bit: http://refspecs.linux-foundation.org/elf/elfspec_ppc.pdf
+# They are sufficiently similar
+# There are some more kernel level registers defined in the ABI at #356 and further, not listed here
+_REG_NAMES_POWERPC = ['r%d' % n for n in range(32)] +[
+      'f%d' % n for n in range(32)] +[
+    'cr', 'fpscr', 'msr', '<none>', '<none>', '<none>',
+    'sr0', 'sr1', 'sr2', 'sr3', 'sr4', 'sr5', 'sr6', 'sr7',
+    'sr8', 'sr9', 'sr10', 'sr11', 'sr12', 'sr13', 'sr14', 'sr15'] + ['<none>']*14 + [
+    'mq', 'xer', '<none>', '<none>', 'rtcu', 'rtcl', '<none>', '<none>',
+    'lr', 'ctr', '<none>', '<none>', '<none>', '<none>', '<none>', '<none>',
+    '<none>', '<none>', 'dsisr', 'dar', '<none>', '<none>', 'dec', '<none>',
+    '<none>', 'sdr1', 'srr0', 'srr1']
 
 # More? 
 
@@ -38,22 +61,25 @@ _REG_NAMES_MIPS = [
 # For PE, see IMAGE_FILE_MACHINE in filebytes.pe
 # For MachO, see make_macho_arch_name() in formats.py, which derives from CpuType under filebytes.mach_o and subtypes
 
-_REG_NAME_MAP = dict(
-    x86 = _REG_NAMES_x86,
-    I386 = _REG_NAMES_x86,
-    x64 = _REG_NAMES_x64,
-    AMD64 = _REG_NAMES_x64,
-    X86_64 = _REG_NAMES_x64,
-    ARM = _REG_NAMES_ARM,
-    ARMV6 = _REG_NAMES_ARM,
-    ARMV7 = _REG_NAMES_ARM,
-    ARMV7A = _REG_NAMES_ARM,
-    ARMV7S = _REG_NAMES_ARM,
-    AArch64 = _REG_NAMES_AArch64,
-    ARM64 = _REG_NAMES_AArch64,
-    ARME = _REG_NAMES_AArch64,
-    MIPS = _REG_NAMES_MIPS
-)
+_REG_NAME_MAP = {
+    'x86': _REG_NAMES_x86,
+    'I386': _REG_NAMES_x86,
+    'x64': _REG_NAMES_x64,
+    'AMD64': _REG_NAMES_x64,
+    'X86_64': _REG_NAMES_x64,
+    'ARM': _REG_NAMES_ARM,
+    'ARMV6': _REG_NAMES_ARM,
+    'ARMV7': _REG_NAMES_ARM,
+    'ARMV7A': _REG_NAMES_ARM,
+    'ARMV7S': _REG_NAMES_ARM,
+    'AArch64': _REG_NAMES_AArch64,
+    'ARM64': _REG_NAMES_AArch64,
+    'ARME': _REG_NAMES_AArch64,
+    'MIPS': _REG_NAMES_MIPS,
+    'RISC-V': _REG_NAMES_RISCV,
+    'PowerPC': _REG_NAMES_POWERPC,
+    '64-bit PowerPC': _REG_NAMES_POWERPC
+}
 
 class ExprFormatter:
     # Operator codes differ in DWARFv1, thus the need for version
