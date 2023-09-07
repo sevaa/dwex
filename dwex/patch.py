d5823985da0ebe244fac0b7b322f7e3d4a9bb833
Seva Alekseyev <sevaa@nih.gov>
2023-09-07 15:03:20 -0400

    More attributes

--- dwex/patch.py
+++ dwex/patch.py
@@ -2,4 +2,6 @@ import elftools.dwarf.enums
 
 def monkeypatch():
     #https://docs.hdoc.io/hdoc/llvm-project/e051F173385B23DEF.html
+    elftools.dwarf.enums.ENUM_DW_AT["DW_AT_LLVM_apinotes"] = 0x3e07
+    elftools.dwarf.enums.ENUM_DW_AT["DW_AT_APPLE_objc_direct"] = 0x3fee
     elftools.dwarf.enums.ENUM_DW_AT["DW_AT_APPLE_sdk"] = 0x3fef
