fc2cc425e2d4b24851c24cf839a7043dadcfa63b
Seva Alekseyev <sevaa@yarxi.ru>
2024-06-14 11:03:05 -0400

    Ver bump

--- dwex/__main__.py
+++ dwex/__main__.py
@@ -11,7 +11,7 @@ from .ui import setup_ui
 from .locals import LocalsDlg
 
 # Sync with version in setup.py
-version = (3, 24)
+version = (3, 25)
 
 # TODO:
 # On MacOS, start without a main window, instead show the Open dialog
