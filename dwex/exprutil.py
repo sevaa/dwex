f2c3cf61a1af62c82a67a6a6cc19f35070ce1159
Seva Alekseyev <sevaa@yarxi.ru>
2024-07-05 10:34:35 -0400

    Typo

--- dwex/exprutil.py
+++ dwex/exprutil.py
@@ -35,7 +35,7 @@ _REG_NAMES_RISCV = [
     'zero', 'ra', 'sp', 'gp', 'tp', 't0', 't1', 't2',
     'fp', 's1', 'a0', 'a1', 'a2', 'a3', 'a4', 'a5',
     'a6', 'a7', 's2', 's3', 's4', 's5', 's6', 's7',
-    't8', 't9', 's10', 's11', 't3', 't4', 't5', 't6'] + [
+    's8', 's9', 's10', 's11', 't3', 't4', 't5', 't6'] + [
         'f%d' % n for n in range(32)] + ['afrc'] + ['<none>']*31 + [
         'v%d' % n for n in range(32)]
 
